18a0655bdd1f12fffd5f21a80ad737cd60686368
gerritt <gmocore@gmail.com>
2020-05-01 11:57:34 -0600

    added api routes

--- app.py
+++ app.py
@@ -1,6 +1,53 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
+CORS(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(200), nullable=False)
+    public_id = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, title, genre, image_url, public_id):
+        self.title = title
+        self.genre = genre
+        self.image_url = image_url
+        self.public_id = public_id
+
+class MovieSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "genre", "image_url", "public_id")
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+@app.route("/api/v1/movie", methods=["POST"])
+def add_movie():
+    title = request.json["title"]
+    genre = request.json["genre"]
+    image_url = request.json["image_url"]
+    public_id = request.json["public_id"]
+
+    new_movie = Movie(title, genre, image_url, public_id)
+
+    db.session.add(new_movie)
+    db.session.commit()
+
+    movie = Movie.query.get(new_movie.id)
+    return movie_schema.jsonify(movie)
 
 @app.route("/")
 def hello():
